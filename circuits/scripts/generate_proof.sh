9645d6f3a6a07b97e1577d8986e60ac72f57c61d
Philipp Piwo <github@piwo.space>
2024-03-04 08:23:08 +0100

    wip stuff

--- circuits/scripts/generate_proof.sh
+++ circuits/scripts/generate_proof.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-mkdir -p bls12_381
+echo "generating proof"
+node build/proof_of_passport_js/generate_witness.js build/proof_of_passport_js/proof_of_passport.wasm build/test_input.json build/test.wtns
 
-circom circuits/proof_of_passport.circom -l node_modules --prime bls12381 --r1cs --wasm --output bls12_381 --O2
+snarkjs wtns check build/proof_of_passport.r1cs build/test.wtns
 
-yarn snarkjs groth16 setup bls12_381/proof_of_passport.r1cs pot20_final.ptau bls12_381/proof_of_passport.zkey
+snarkjs groth16 prove build/proof_of_passport_final.zkey build/test.wtns build/test_proof.json build/test_public.json
 
-yarn snarkjs zkey export verificationkey bls12_381/proof_of_passport.zkey bls12_381/verification_key.json
+cp build/test_proof.json ../ae-contracts/test/test_proof.json
+cp build/test_public.json ../ae-contracts/test/test_public.json
 
-node bls12_381/proof_of_passport_js/generate_witness.js bls12_381/proof_of_passport_js/proof_of_passport.wasm bls12_381/test_input.json bls12_381/test.wtns
-
-snarkjs wtns check bls12_381/proof_of_passport.r1cs bls12_381/test.wtns
-
-snarkjs groth16 prove bls12_381/proof_of_passport.zkey bls12_381/test.wtns bls12_381/test_proof.json bls12_381/test_public.json
-
-cp bls12_381/test_proof.json ../ae-contracts/test/test_proof.json
-cp bls12_381/test_public.json ../ae-contracts/test/test_public.json
+echo "generated proof"
